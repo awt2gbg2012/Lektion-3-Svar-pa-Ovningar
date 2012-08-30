d21be9cde5e0223f254e5182a3e8a1e06513066b
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-30 20:38:27 +0200

    Uppgift 13

--- Lektion3SvarOvningar/Program.cs
+++ Lektion3SvarOvningar/Program.cs
@@ -12,9 +12,24 @@ namespace Lektion3SvarOvningar
     {
         static void Main(string[] args)
         {
-            var Repo = new Repository();
-            foreach (var user in Repo.GetUsers(u => u.Type == User.UserType.SuperUser))
-                Print(user.ToString());
+            Print(@"
+Det krävs eg. ingenting.
+
+T representerar en generisk typ - dvs. den kan ersättas av vilken typ som helst
+
+Vad gäller .Contains(..) som används i funktionen så kräver den att typen som 
+används skall implementera interfacet IEquatable. 
+
+Dock så ärver alla objekt från System.Object, som implementerar IEquatable.
+Dvs. ifall inte typen som används implementerar IEquatable så kommer den
+falla tillbaka på Objects Equals()-metod. 
+
+Detta innebär att för att true skall returneras så måste exakt samma objekt
+finnas med i listan (eller rättare sagt en referens till samma objekt). T.ex,
+för User-objekt, så räcker det inte ifall en User med samma username finns med 
+i listan.
+
+            ");
 
             Console.ReadLine();
         }
