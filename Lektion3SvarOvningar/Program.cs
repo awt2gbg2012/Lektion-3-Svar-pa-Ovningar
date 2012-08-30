219fdf4d5b1bcead1e21adc6adc0b3e601975819
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-30 19:25:25 +0200

    Innan några uppgifter är lösta
    Take 2!

---
+++ Lektion3SvarOvningar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lektion3SvarOvningar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadLine();
+        }
+
+        // Hjälpmetod för att kunna snabba på då jag vill skriva ut ngt till konsolen
+        static void Print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
