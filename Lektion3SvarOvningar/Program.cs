622c3da8103a0655a2690fe5def953e4a984d774
David Gullmarsvik <awt2.2012@gmail.com>
2012-08-30 20:14:48 +0200

    Uppgift 9

--- Lektion3SvarOvningar/Program.cs
+++ Lektion3SvarOvningar/Program.cs
@@ -12,12 +12,19 @@ namespace Lektion3SvarOvningar
     {
         static void Main(string[] args)
         {
-            var allUsers = (new Repository()).GetUsers();
-            var allPosts = (new Repository()).GetPosts();
+            // Insåg att om jag skapar ett repo för bägge raderna nedan så kommer 
+            // användarna ha olika IDs i allPosts och allUsers eftersom datan genereras 
+            // om varje gång jag skapa ett repo - är även ett problem för min lösning på 
+            // uppgift 6 - använd samma repo för både .GetUsers() och .GetPosts() för 
+            // att få ut ett resultat för uppg. 6
+            var Repo = new Repository(); 
+            var allUsers = Repo.GetUsers();
+            var allPosts = Repo.GetPosts();
 
-            var averageLengthForFullName = allUsers.Average(u => u.FullName.Length);
-            Print(averageLengthForFullName.ToString());
-
+            var funnyPosts = allPosts.Where(p => p.Tags.Any(t => t == Post.PostTags.Funny));
+            var postersOfFunnyTags = allUsers.Where(u => funnyPosts.Any(p => p.CreatedByID == u.UserID));
+            foreach (var user in postersOfFunnyTags)
+                Print(user.ToString());
 
 
             Console.ReadLine();
